8282494c916da4ff5f5f2ac25bfa189cd819ed5b
Noah Albers <130871651+NoahAlbers@users.noreply.github.com>
2026-09-24 14:01:42 -0400

    Upgrade Express to 5.2.1 and adapt account routes

--- CubitServices/src/app.ts
+++ CubitServices/src/app.ts
@@ -112,7 +112,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 })
 
 //catch bad routes here
-app.use('*', (req, res, next) => {
+app.use((req, res, next) => {
   res.status(404).json({ message: 'Not Found' })
 })
 
